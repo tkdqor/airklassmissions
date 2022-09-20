018a42591bd8437fe92a8372f1775aef217f0e72
taxijjang <gw9122@naver.com>
2022-09-20 14:47:38 +0900

    모델 수정

--- djangomission/contentshub/models.py
+++ djangomission/contentshub/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Master(models.Model):
-    user = models.ForeignKey('accounts.User', on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    name = models.CharField(max_length=10)
 
 
 class Klass(models.Model):
-    master = models.ForeignKey('contentshub.Master', on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    master = models.ForeignKey('contentshub.Master', on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
