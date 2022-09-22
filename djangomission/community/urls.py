df8fa8f46ab8158946299903d5e3e35bf433a16f
sangbaek <aumsbk@naver.com>
2022-09-22 23:34:48 +0900

    Feat : #4 유저가 생성한 질문 삭제 및 복구 API 개발 - 유저가 생성한 질문을 삭제 및 복구할 수 있도록 PATCH method 선택 및 is_deleted 필드 수정 - JSON 형태의 is_deleted Boolean 값을 요청하여 특정 질문을 삭제하거나 복구 - community 앱에 QuestionDeleteAPIView 생성

--- djangomission/community/urls.py
+++ djangomission/community/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import QuestionCreateAPIView
+from .views import QuestionCreateAPIView, QuestionDeleteAPIView
 
 app_name = "community"
 
 urlpatterns = [
     path("api/v1/klasses/<klass_id>", QuestionCreateAPIView.as_view()),
+    path("api/v1/questions/<question_id>", QuestionDeleteAPIView.as_view()),
 ]
